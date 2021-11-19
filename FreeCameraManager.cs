e1deaaedbbfe507bc11c87786eba975ac355e035
Xabier <xajubdev@gmail.com>
2021-11-19 17:38:30 +0100

    Zoom movement added to camera

--- FreeCameraManager.cs
+++ FreeCameraManager.cs
@@ -4,47 +4,62 @@ using UnityEngine;
 
 public class FreeCameraManager : MonoBehaviour
 {
-    public float speed = 1f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float axiSpeed = 100f;
+    public float zoomSpeed = 400f;
+    public float scrollSensibility = 1f;
+    public float minZoom = 10;
+    public float maxZoom = 100;
 
     // Update is called once per frame
     void Update()
     {
         AxisMovement();
-        ZoomMovement();
+        Zoom();
+        Rotation();
     }
-
 
     private void AxisMovement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
+        
         if(vertical != 0) // w/s or Up/down movement
         {
             float angle = Mathf.Deg2Rad * transform.localEulerAngles.y;
-            float distanceX = speed * Mathf.Sin(angle) * vertical;
-            float distanceZ = speed * Mathf.Cos(angle) * vertical;
-            transform.position = new Vector3(transform.position.x + distanceX, transform.position.y , transform.position.z + distanceZ);
+            Vector3 movement = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * vertical * axiSpeed * Time.deltaTime;
+            transform.Translate(movement,Space.World);
 
         }
-        if(horizontal != 0) // a/d or left/right movement
+        if (horizontal != 0) // a/d or left/right movement
         {
             float angle = Mathf.Deg2Rad * (transform.localEulerAngles.y + 90); // The perpendicular angle
-            float distanceX = speed * Mathf.Sin(angle) * horizontal;
-            float distanceZ = speed * Mathf.Cos(angle) * horizontal;
-
-            transform.position = new Vector3(transform.position.x + distanceX, transform.position.y, transform.position.z + distanceZ);
+            Vector3 movement = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * horizontal * axiSpeed * Time.deltaTime;
+            transform.Translate(movement, Space.World);
         }
 
     }
-    private void ZoomMovement()
-    {
 
+    private void Zoom()
+    {
+        float mouseScroll = Input.mouseScrollDelta.y;
+        if(mouseScroll != 0)
+        {
+            float zoomLevel = mouseScroll * scrollSensibility;
+            Vector3 zooMovement = transform.forward * zoomLevel * zoomSpeed * Time.deltaTime;
+
+            bool insideScope = (transform.position.y < maxZoom && transform.position.y > minZoom);
+            bool increasingToScope = (transform.position.y < minZoom && zooMovement.y > 0);
+            bool decreasingToScope = (transform.position.y > maxZoom && zooMovement.y < 0);
+            if (insideScope || increasingToScope || decreasingToScope)
+            {
+                transform.Translate(zooMovement,Space.World);
+            }
+        }
     }
+    private void Rotation()
+    {
+        //TO DO
+    }
+
 }
